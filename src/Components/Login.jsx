3ec65f9a4b41b99ec46fcda772f5e316a176dcc2
ikujebikehinde <ikujebikehinde@gmail.com>
2023-09-20 08:33:35 -0700

    login

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -88,7 +88,7 @@ const Signin = ({setIsAuthenticated}) => {
       </div>
 
       <div className="  justify-center m-auto my-[4rem] items-center bg-white w-[25rem]">
-        <div className="text-center mt-[10rem]  text-2xl font-bold">
+        <div className="text-center text-gray-600 mt-[10rem]  text-2xl font-bold">
           <h1>Sign In</h1>
         </div>
         <div className="block justify-center items-center flex-col  h-80 mt-10 ">
@@ -154,13 +154,13 @@ const Signin = ({setIsAuthenticated}) => {
                     loading={loading}
                     type="primary"
                     htmlType="submit"
-                    className="bg-[#134c98] flex items-center justify-center py-5"
+                    className="bg-gray-200 text-gray-700 flex items-center justify-center py-5"
                     block
                   >
                     Sign In
                   </Button>
                 </Col>
-                <Link to={'/create'} className="text-sm font-normal mb-2 pl-[1rem] text-[#75C2F6]">
+                <Link to={'/create'} className="text-sm font-normal mb-2 pl-[1rem] text-gray-500">
                   <h6>create user</h6>
                 </Link>
               </Row>
